f34e3cd102108fc8625f5806f3f1852d7ad233ce
windsmoon <smwindsmoon@gmail.com>
2026-09-01 21:45:57 +0800

    data

--- UnityProject/Assets/Runtime/DestructibleCell.cs
+++ UnityProject/Assets/Runtime/DestructibleCell.cs
@@ -1,18 +1,14 @@
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 namespace Windsmoon.DesctructibleBoard
 {
+    [Serializable]
     public struct DestructibleCell
     {
         #region fields
         public int Id;
         public Vector2 Site;
-        public List<Vector2> Polygon;
-        public List<int> Neighbors;
-        public Mesh Mesh;
-        public Collider Collider;
-        public bool Destroyed;
         #endregion
     }
 }
---
+++ UnityProject/Assets/Runtime/LocalDestructiblePanel.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Windsmoon.DesctructibleBoard
+{
+    public class LocalDestructiblePanel : MonoBehaviour
+    {
+        #region fields
+        [Header("Panel")]
+        [SerializeField, Min(0.01f)] 
+        private float _width = 10f;
+        [SerializeField, Min(0.01f)] 
+        private float _height = 6f;
+        [SerializeField, Min(0.01f)] 
+        private float _thickness = 0.2f;
+
+        [Header("Fragments")]
+        [SerializeField, Min(0.01f)] 
+        private float _fragmentSize = 0.5f;
+        [SerializeField] 
+        private int _seed = 1;
+        [SerializeField, Min(1)] 
+        private int _maxFragmentCount = 300;
+
+        [SerializeField, HideInInspector] 
+        private List<DestructibleCell> _cellList = new List<DestructibleCell>();
+        #endregion
+
+        #region properties
+        public IReadOnlyList<DestructibleCell> CellList => _cellList;
+        #endregion
+
+        #region unity methods
+        private void OnDrawGizmos()
+        {
+            Matrix4x4 previousMatrix = Gizmos.matrix;
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.DrawWireCube(Vector3.zero, new Vector3(_width, _height, _thickness));
+            Gizmos.matrix = previousMatrix;
+        }
+        #endregion
+    }
+}
