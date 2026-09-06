cff1873acdf6dbf6f0e669ddca0f7d650363e3e7
windsmoon <smwindsmoon@gmail.com>
2026-09-06 20:44:24 +0800

    grid data

---
+++ UnityProject/Assets/Runtime/GridData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Windsmoon.DesctructibleBoard
+{
+    [Serializable]
+    internal class GridData
+    {
+        #region fields
+        [SerializeField]
+        private int[] _bucketOffsets;
+        [SerializeField]
+        private int[] _cellIds;
+        #endregion
+
+        #region methods
+
+        internal void Build(List<DestructibleCell> cellList)
+        {
+        }
+
+        public bool TryGetCellIndex(Vector2 position, out int index)
+        {
+            index = -1;
+            return false;
+        }
+        #endregion
+    }
+}
